3cf98ebfb63f972998879119897972ba120e397c
BuildTools <unconfigured@null.spigotmc.org>
2023-11-01 15:06:02 +0900

    test2

--- src/main/java/cc/chocomint/nAddon/Main.java
+++ src/main/java/cc/chocomint/nAddon/Main.java
@@ -1,11 +1,9 @@
 package cc.chocomint.nAddon;
 
-import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 	
 	public void onEnable() {
-		Bukkit.getServer().broadcastMessage("test");
 	}
 }
