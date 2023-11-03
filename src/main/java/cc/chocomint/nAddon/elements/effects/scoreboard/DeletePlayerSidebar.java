b538b10f8cd9cbfec609392ea4e87a24d3f482e5
BuildTools <unconfigured@null.spigotmc.org>
2023-11-03 10:35:01 +0900

    スコアボード周りのクラス追加

---
+++ src/main/java/cc/chocomint/nAddon/elements/effects/scoreboard/DeletePlayerSidebar.java
@@ -0,0 +1,50 @@
+package cc.chocomint.nAddon.elements.effects.scoreboard;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+public class DeletePlayerSidebar extends Effect {
+	
+	private Expression<Player> ex_player;
+	
+	static {
+		Skript.registerEffect(DeletePlayerSidebar.class, "delete %player%'s sidebar");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		
+		this.ex_player = (Expression<Player>) exprs[0];
+		
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return "DeletePlayerSidebar";
+	}
+
+	@Override
+	protected void execute(Event e) {
+		
+		Player player = (Player) this.ex_player.getSingle(e);
+		Objective object = player.getScoreboard().getObjective(DisplaySlot.SIDEBAR);
+		
+		if(object != null) {
+			object.unregister();
+		}
+		
+	}
+
+}
---
+++ src/main/java/cc/chocomint/nAddon/elements/effects/scoreboard/EntryTeam.java
@@ -0,0 +1,49 @@
+package cc.chocomint.nAddon.elements.effects.scoreboard;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.scoreboard.Team;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Effect;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.util.Kleenean;
+
+public class EntryTeam extends Effect {
+	
+	private Expression<Player> ex_player;
+	private Expression<Team> ex_team;
+	
+	static {
+		Skript.registerEffect(EntryTeam.class, "entry %player% to %team%");
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
+		
+		this.ex_player = (Expression<Player>) exprs[0];
+		this.ex_team = (Expression<Team>) exprs[1];
+		
+		return true;
+	}
+
+	@Override
+	public String toString(@Nullable Event e, boolean debug) {
+		return "EntryTeam";
+	}
+
+	@Override
+	protected void execute(Event e) {
+		
+		Player player = (Player) ex_player.getSingle(e);
+		Team team = (Team) ex_team.getSingle(e);
+		
+		team.addEntry(player.getName());
+		
+	}
+
+}
