8efa51f57c869ce5f5f2b0cf5d8b5ed2375df2b2
MovingEmoji <mcnana814@gmail.com>
2025-03-31 01:08:47 +0900

    WebAPI用の動的変数の実装

--- src/main/java/cc/chocomint/nAddon/customevents/CreatedDynamicValueEvent.java
+++ src/main/java/cc/chocomint/nAddon/customevents/CreatedDynamicValueEvent.java
@@ -2,11 +2,13 @@ package cc.chocomint.nAddon.customevents;
 
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
+import org.json.simple.JSONObject;
 
 public class CreatedDynamicValueEvent extends Event {
 	
 	public static final HandlerList HANDLERS = new HandlerList();
 	private final String key;
+	private final Object object;
 	
 	public static HandlerList getHandlerList() {
 		return HANDLERS;
@@ -17,14 +19,17 @@ public class CreatedDynamicValueEvent extends Event {
 		return HANDLERS;
 	}
 	
-	public CreatedDynamicValueEvent(String key) {
+	public CreatedDynamicValueEvent(String key, Object object) {
 		
 		this.key = key;
-		
+		this.object = object;
 	}
 	
 	public String getKey() {
 		return this.key;
 	}
 
+	public Object getJson() {
+		return this.object;
+	}
 }
