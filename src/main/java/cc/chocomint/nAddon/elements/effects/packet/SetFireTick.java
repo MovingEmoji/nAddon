07b74ee32900581d5b050bb2ab286afbaabda285
MovingEmoji <mcnana814@gmail.com>
2025-03-31 01:10:48 +0900

    階層変更

--- src/main/java/cc/chocomint/nAddon/elements/effects/packet/SetFireTick.java
+++ src/main/java/cc/chocomint/nAddon/elements/effects/packet/SetFireTick.java
@@ -1,4 +1,4 @@
-package cc.chocomint.nAddon.elements.effects;
+package cc.chocomint.nAddon.elements.effects.packet;
 
 import javax.annotation.Nullable;
 
@@ -12,12 +12,14 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser.ParseResult;
 import ch.njol.util.Kleenean;
 
-public class RespawnPlayer extends Effect {
+public class SetFireTick extends Effect {
 	
 	private Expression<Player> ex_player;
+	private Expression<Number> ex_tick;
 	
 	static {
-		Skript.registerEffect(RespawnPlayer.class, "force respawn %player%");
+		Skript.registerEffect(SetFireTick.class, "set %player%'s fire tick to %number%");
+		Skript.registerEffect(SetFireTick.class, "set fire tick of %player% to %number%");
 		Main.Effects ++;
 	}
 
@@ -26,21 +28,23 @@ public class RespawnPlayer extends Effect {
 	public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, ParseResult parseResult) {
 		
 		this.ex_player = (Expression<Player>) exprs[0];
+		this.ex_tick = (Expression<Number>) exprs[1];
 		
 		return true;
 	}
 
 	@Override
 	public String toString(@Nullable Event e, boolean debug) {
-		return "RespawnPlayer";
+		return "SetFireTick";
 	}
 
 	@Override
 	protected void execute(Event e) {
 		
 		Player player = (Player) this.ex_player.getSingle(e);
+		Number tick = (Number) this.ex_tick.getSingle(e);
 		
-		player.spigot().respawn();
+		player.setFireTicks(tick.intValue());
 		
 	}
 
